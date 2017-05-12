8a0799cdaa9a915342c9867a417d849e9105c1d5
Szymon Kazmierczak <szymonkaz@gmail.com>
2017-05-12 12:11:45 +0100

    Added two headers without which I was unable to establish a connection with the Twitch API. As the Key is private, I added it as a build config var that has to be added to the global gradle.properties. Also updated gradle in the process and removed unnecessary class GameListDeserializer

--- app/src/main/java/org/simonsays/retrofitguide/rest/ApiClient.java
+++ app/src/main/java/org/simonsays/retrofitguide/rest/ApiClient.java
@@ -1,5 +1,13 @@
 package org.simonsays.retrofitguide.rest;
 
+import org.simonsays.retrofitguide.BuildConfig;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,15 +16,38 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public class ApiClient {
 
-    public static final String BASE_URL = "https://api.twitch.tv/kraken/";
+    private static final String BASE_URL = "https://api.twitch.tv/kraken/";
     private static Retrofit retrofit = null;
 
 
+
     public static Retrofit getClient() {
+
         if (retrofit==null) {
+
+            // Add headers
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            httpClient.addInterceptor(new Interceptor() {
+                @Override
+                public Response intercept(Interceptor.Chain chain) throws IOException {
+                    Request original = chain.request();
+
+                    Request request = original.newBuilder()
+                            .header("Client-ID", BuildConfig.TwitchApiKey)
+                            .header("Accept", "application/vnd.twitchtv.v5+json")
+                            .method(original.method(), original.body())
+                            .build();
+
+                    return chain.proceed(request);
+                }
+            });
+
+            OkHttpClient client = httpClient.build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
         return retrofit;
